81fb34f43e62a8eef649a35eed52c809a9cd0752
leoseis <eboscrib2@yahoo.com>
2024-03-05 12:41:55 +0100

    views added and mapped with url

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
+def rooms(request):
+    return render (request, 'home.html')
 # Create your views here.
--- wechatapp/urls.py
+++ wechatapp/urls.py
@@ -21,5 +21,6 @@ from django.views.generic.base import TemplateView  # new
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path('',include('chatapp.urls')),
+
 ]
